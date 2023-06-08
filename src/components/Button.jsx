b05d1ce97fe9a90f4765acc49fea9b28228dfa18
Heurtault Aubin <aub.heurtault@gmail.com>
2023-06-08 10:04:32 +0200

    add new option

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Text, View, StyleSheet, Pressable } from 'react-native';
 
-export default function Button({ title, onPress }) {
+export default function Button({ title, onPress, fullRound, adjustSize }) {
+    const buttonStyles = [
+        styles.button,
+        fullRound && styles.fullRound,
+        adjustSize && styles.adjustSize,
+    ];
+
     return (
-        <Pressable style={styles.button} onPress={onPress}>
+        <Pressable style={buttonStyles} onPress={onPress}>
             <Text style={styles.text}>{title}</Text>
         </Pressable>
     );
@@ -19,6 +25,12 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         elevation: 3,
         backgroundColor: '#000000',
+    },
+    fullRound: {
+        borderRadius: 100, // Utiliser une valeur suffisamment élevée pour obtenir un arrondi complet
+    },
+    adjustSize: {
+        width: 'auto', // Ajuster la largeur en fonction du contenu
     },
     text: {
         fontSize: 16,
